561f553c6e72cc922c810fd6faf4fe21e3a92760
laozhou <170972840@qq.com>
2019-01-06 14:53:17 +0000

    完成用户注册功能

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+}
